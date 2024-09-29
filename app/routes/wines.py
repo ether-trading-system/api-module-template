357595a9aa6525adde0548b8af5bb1b21e42664f
corn <dalcon10280@gmail.com>
2024-09-29 14:15:39 +0900

    temp

--- app/routes/wines.py
+++ app/routes/wines.py
@@ -1,8 +1,34 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from core import get_session
+from domain.wine.wine_service import WineService
 
 router = APIRouter(prefix="/wines", tags=["wines"])
 
 
 @router.get("")
-async def read_users():
+async def get_wines(
+        session: AsyncSession = Depends(get_session)
+):
+    _service = WineService(session)
+    return await _service.get_all_wines()
+
+
+@router.get("")
+async def get_wine():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@router.post("", status_code=201)
+async def create_wine():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@router.put("")
+async def update_wine():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@router.delete("", status_code=204)
+async def delete_wine():
     return [{"username": "Rick"}, {"username": "Morty"}]
